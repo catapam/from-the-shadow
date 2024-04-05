4695dd457524bc698ca941820da360c99d8ca5c4
Matheus Andrade <matheuscatapam@gmail.com>
2024-04-05 13:57:51 +0000

    add functionalities to buttons start and info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,37 @@
-function start(){
+document.addEventListener('DOMContentLoaded', function() {
+    var infoButton = document.getElementById('info_button');
+    var modal = document.getElementById('info_modal');
+    var closeButton = document.getElementsByClassName('close')[0];
+    var startButton = document.getElementById('start_button');
+    var startArea = document.getElementById('start_area');
+    var nameArea = document.getElementById('name');
+    var body = document.body;
+    var heroNameInput = document.getElementById('id');
 
-}
+    infoButton.addEventListener('click', function() {
+        modal.style.display = 'block';
+    });
 
-function info(){
-    
-}
+    closeButton.addEventListener('click', function() {
+        modal.style.display = 'none';
+    });
+
+    window.addEventListener('click', function(event) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    });
+
+    startButton.addEventListener('click', function() {
+        startArea.style.display = 'none';
+        nameArea.style.display = 'block';
+        body.style.backgroundImage='radial-gradient(circle at 75vw 35vh,rgba(0, 0, 0, 1) 0%,rgba(0, 0, 0, 1) 10%, rgba(0, 0, 0, 0.50) 100%),url(/assets/images/game_background.png)';
+    });
+
+    heroNameInput.addEventListener('blur', function() {
+        var inputValue = heroNameInput.value;
+        if(inputValue) {
+            heroNameInput.value = inputValue.charAt(0).toUpperCase() + inputValue.slice(1);
+        }
+    });
+});
