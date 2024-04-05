5989ac6bcbc9b3e06dc46673b9d23b2243a9d68d
Matheus Andrade <matheuscatapam@gmail.com>
2024-04-05 16:47:11 +0000

    add caracters to the fight screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var infoButton = document.getElementById('info_button');
     var modal = document.getElementById('info_modal');
     var closeButton = document.getElementsByClassName('close')[0];
@@ -7,31 +7,56 @@ document.addEventListener('DOMContentLoaded', function() {
     var nameArea = document.getElementById('name');
     var body = document.body;
     var heroNameInput = document.getElementById('id');
+    var fightButton = document.getElementById('fight_button');
+    var fightArea = document.getElementById('fight_area');
 
-    infoButton.addEventListener('click', function() {
+    infoButton.addEventListener('click', function () {
         modal.style.display = 'block';
     });
 
-    closeButton.addEventListener('click', function() {
+    closeButton.addEventListener('click', function () {
         modal.style.display = 'none';
     });
 
-    window.addEventListener('click', function(event) {
+    window.addEventListener('click', function (event) {
+        event.preventDefault();
         if (event.target == modal) {
             modal.style.display = 'none';
         }
     });
 
-    startButton.addEventListener('click', function() {
+    startButton.addEventListener('click', function () {
         startArea.style.display = 'none';
         nameArea.style.display = 'block';
-        body.style.backgroundImage='radial-gradient(circle at 75vw 35vh,rgba(0, 0, 0, 1) 0%,rgba(0, 0, 0, 1) 10%, rgba(0, 0, 0, 0.50) 100%),url(/assets/images/game_background.png)';
+        body.style.backgroundImage = 'radial-gradient(circle at 75vw 35vh,rgba(0, 0, 0, 1) 0%,rgba(0, 0, 0, 1) 10%, rgba(0, 0, 0, 0.50) 100%),url(/assets/images/game_background.png)';
     });
 
-    heroNameInput.addEventListener('blur', function() {
-        var inputValue = heroNameInput.value;
-        if(inputValue) {
-            heroNameInput.value = inputValue.charAt(0).toUpperCase() + inputValue.slice(1);
+    heroNameInput.addEventListener('blur', function () {
+        var inputName = heroNameInput.value;
+        if (inputName) {
+            heroNameInput.value = inputName.charAt(0).toUpperCase() + inputName.slice(1);
         }
     });
+
+    fightButton.addEventListener('click', function () {
+        nameArea.style.display = 'none';
+        fightArea.style.display = 'flex';
+     });
 });
+
+function attack(){
+
+};
+
+function fire(){
+
+};
+
+function recharge(){
+
+};
+
+function item(){
+
+};
+
